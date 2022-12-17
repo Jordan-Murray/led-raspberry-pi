4abc723d7e97d7fbd7e5da973e3627b1e49881a9
Jordan Murray <jlmurray100@gmail.com>
2022-12-17 18:43:09 +0000

    Adding stuff to dashboard folder

--- src/dashboard/main-dash.py
+++ src/dashboard/main-dash.py
@@ -1,44 +1,54 @@
 import time
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from matrix import Matrix
+from utils import args, led_matrix_options
 from brightness_control import control_brightness
 from temp_display import display_temp
 
-# Set up the options for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
-options.chain_length = 1
-options.parallel = 1
-options.pwm_bits = 11
-options.brightness = 60
-options.pwm_lsb_nanoseconds = 130
-options.led_rgb_sequence = "RGB"
-options.led_gpio_mapping = "adafruit-hat"
+def run():
 
-# Create the matrix object with the options
-matrix = RGBMatrix(options = options)
+    # Get supplied command line arguments
+    commandArgs = args()
+    # Check for led configuration arguments
+    matrixOptions = led_matrix_options(commandArgs)
+    matrixOptions.drop_privileges = False
 
-# Set the temperature update interval to 5 minutes (in seconds)
-temp_update_interval = 5 * 60
+    # Set up the options for the matrix
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 64
+    options.chain_length = 1
+    options.parallel = 1
+    options.pwm_bits = 11
+    options.brightness = 60
+    options.pwm_lsb_nanoseconds = 130
+    options.led_rgb_sequence = "RGB"
+    options.led_gpio_mapping = "adafruit-hat"
 
-try:
-    while True:
-        # Get the current time
-        current_time = time.strftime("%I:%M %p")
+    # Create the matrix object with the options
+    matrix = RGBMatrix(options = options)
 
-        # Clear the matrix
+    # Set the temperature update interval to 5 minutes (in seconds)
+    temp_update_interval = 5 * 60
+
+    try:
+        while True:
+            # Get the current time
+            current_time = time.strftime("%I:%M %p")
+
+            # Clear the matrix
+            matrix.Clear()
+
+            # Draw the time on the matrix
+            matrix.DrawText(current_time, x=0, y=0, font=matrix.Font6x10, color=(255,255,255))
+
+            # Call the control_brightness function to adjust the brightness if necessary
+            control_brightness(matrix)
+
+            # Call the display_temp function to display the current temperature on the matrix
+            display_temp(matrix, temp_update_interval)
+            
+
+    except KeyboardInterrupt:
         matrix.Clear()
-
-        # Draw the time on the matrix
-        matrix.DrawText(current_time, x=0, y=0, font=matrix.Font6x10, color=(255,255,255))
-
-        # Call the control_brightness function to adjust the brightness if necessary
-        control_brightness(matrix)
-
-        # Call the display_temp function to display the current temperature on the matrix
-        display_temp(matrix, temp_update_interval)
-        
-
-except KeyboardInterrupt:
-    matrix.Clear()
-    matrix.Terminate()
+        matrix.Terminate()
