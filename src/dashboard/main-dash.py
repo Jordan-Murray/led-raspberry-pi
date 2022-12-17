0079fd03bd41825493ae5d97fd14f8df7332f14a
Jordan Murray <jlmurray100@gmail.com>
2022-12-17 18:52:42 +0000

    Testing time

--- src/dashboard/main-dash.py
+++ src/dashboard/main-dash.py
@@ -40,7 +40,7 @@ def run():
         matrix.clear()
 
         # Draw the time on the matrix
-        matrix.draw_text((0,0), current_time, ImageFont.truetype("DejaVuSansMono.ttf", 10), fill=(255,255,255), backgroundColor = (0,0,0))
+        matrix.draw_text((0,0), current_time, ImageFont.truetype("DejaVuSansMono.ttf", 20), fill=(255,255,255), backgroundColor = (0,0,0))
 
         matrix.render()
         # Call the control_brightness function to adjust the brightness if necessary
