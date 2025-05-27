473f92caabb1c7f78ad20338cb33dc8ea391ea18
Jordan Murray <jlmurray100@gmail.com>
2025-05-27 14:18:36 +0100

    Brightness testing

--- src/stock-ticker/main.py
+++ src/stock-ticker/main.py
@@ -14,7 +14,7 @@ def run():
     # Initialize the matrix
     matrix = Matrix(RGBMatrix(options = matrixOptions))
     # Set brightness to 50%
-    matrix.set_brightness(50)
+    matrix.set_brightness(100)
 
     renderer = LayoutRenderer(matrix)
     while True:
